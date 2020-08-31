835d0551845fac6752774fc30fee3174ccb03edd
EC2 Default User <ec2-user@ip-172-31-28-22.us-east-2.compute.internal>
2020-08-31 20:36:43 +0000

    added files and suc2h

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+// Reverses a strong.
+function reverse(string){
+    return string.split("").reverse().join("");
+}
+
+
+//Returns true for a palindrome, false otherwise
+
+function palindrome(string) {
+    let processedContent = string.toLowerCase();
+    return processedContent === reverse(processedContent);
+}
