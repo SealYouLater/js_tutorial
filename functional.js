521d3ff149700f7fb3d1f388ebc81767b5652337
EC2 Default User <ec2-user@ip-172-31-28-22.us-east-2.compute.internal>
2020-09-21 16:22:33 +0000

    added stuff

--- functional.js
+++ functional.js
@@ -27,4 +27,73 @@ function functionalUrls(elements) {
 
 console.log(functionalUrls(states));
 
+
+
 //singles: Imperative version 
+
+function imperativeSingles(elements){
+ let singles = []
+ elements.forEach(function(element) {
+     if (element.split(/\s+/).length ===1){
+         singles.push(element);
+     }
+ });
+ 
+ return singles;   
+}
+
+
+
+//singles: Functional version 
+
+function functionalSingles(elements){
+    return elements.filter(element => element.split(/\s+/).length ===1);
+    
+    
+}
+
+console.log(functionalSingles(states))
+
+// sum imperative solution
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function imperativeSum(elements){
+    let total = 0;
+    elements.forEach(function(n) {
+        total += n;
+    })
+
+    return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum functional solution
+
+
+function functionalSum(elements){
+  return elements.reduce((total, n) => { return total += n;});
+}
+console.log(functionalSum(numbers));
+
+
+
+//  length imperative soluytion
+
+function imperativeLengths(elements){
+    let lengths = {};
+    
+    elements.forEach(function(element){
+        lengths[element] = element.length;
+    });
+    return lengths;
+}
+
+//  length functional soluytion
+
+function functionalLengths(elements){
+    return elements.reduce((lengths, element) => {
+        lengths[element] = element.length;
+        return lengths;
+    }, {});
+}
+console.log(functionalLengths(states));
