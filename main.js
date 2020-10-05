38e449f267147cb6cea8a42535e9026724acd1e2
EC2 Default User <ec2-user@ip-172-31-28-22.us-east-2.compute.internal>
2020-10-05 15:39:08 +0000

    added palindrome detector

---
+++ main.js
@@ -0,0 +1,13 @@
+let Phrase = require("mseal-palindrome");
+
+let string = prompt("please enter a string for palindrome testing");
+
+let phrase= new Phrase(string);
+
+if (phrase.palindrome()){
+    alert(`"${phrase.content}" is a palindrome`);
+    }    else {
+        alert(`"${phrase.content}" is not a palindrome`);
+        
+    }
+
